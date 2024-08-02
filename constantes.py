f6003199a631ead5c5f27fcb97a4c18209adc5ee
Pierre NEILLO <pierre.neillo@free.fr>
2024-08-02 15:32:47 +0200

    Liste des coups

--- constantes.py
+++ constantes.py
@@ -1,1 +1,18 @@
-NB_TOURS = 10  # Bidon
+NB_TOURS = 13
+
+
+restrictions = {
+    "1": {"type": "n_of_a_kind", "kind": 1},
+    "2": {"type": "n_of_a_kind", "kind": 2},
+    "3": {"type": "n_of_a_kind", "kind": 3},
+    "4": {"type": "n_of_a_kind", "kind": 4},
+    "5": {"type": "n_of_a_kind", "kind": 5},
+    "6": {"type": "n_of_a_kind", "kind": 6},
+    "brelan": {"type": "variable", "expr": "aaabc"},
+    "carré": {"type": "variable", "expr": "aaaab"},
+    "full": {"type": "variable", "expr": "aaabb"},
+    "petite_suite": {"type": "variable", "expr": "(1234|2345|3456)."},
+    "grande_suite": {"type": "variable", "expr": "12345|23456"},
+    "yams": {"type": "variable", "expr": "aaaaa"},
+    "chance": {"type": "any"},
+}
--- logique.py
+++ logique.py
@@ -4,26 +4,35 @@ from constantes import *
 class Coup:
     coups = []  # Tous les coups possibles
 
-    def __init__(self, nom, restrictions):
+    def __init__(self, nom, restrictions) -> None:
         self.restrictions = restrictions
         self.nom = nom
+        Coup.coups.append(self)
 
-    def possible(self, des):
+    def est_possible(self, des: list[int]) -> bool:
         """Renvoie un booléen indiquant si le lancer de dés correspond au coup"""
+        pass
 
-    def score(self, des):
+    def score(self, des: list[int]) -> int:
         """Prend en paramètre un lancer de dés correspondant au coup, et renvoie le score obtenu pour ce coup"""
+        pass
 
     @classmethod
-    def coups_possibles(cls, des):
+    def coups_possibles(cls, des: list[int]):
         c_pos = []
         for c in Coup.coups:
             if c.possible(des):
                 c_pos.append(c)
         return c
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.nom
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str(self)
+
+
+def init_logique():
+    # On crée les instances de coup
+    for k, restr in restrictions.items():
+        Coup(k, restr)
