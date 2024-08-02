2c8837dc491c5893c6de200f810d1865b5d80337
Elouan-N <148973555+Elouan-N@users.noreply.github.com>
2024-08-02 14:53:35 +0200

    structure du tour de jeu

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ from constantes import *
 from graphisme import *
 from logique import *
 
+from random import randint
 
-def jouer():
-    pass
+def tirage(n):
+    tirage = []
+    for _ in range(n):
+        tirage.append(randint(1,6))
+    return tirage
+
+def jouer(iTour, joueur):
+    des_conserves = []
+    for iLance in range(3):
+        tirage = tirage(5-len(des_conserves))
+        afficher_des(tirage, des_conserves)
+        # Le joueur enlève ou met des dés de côté
+        # Eventuellement, le joueur d'arrête et indique où il veut mettre les points ou s'il veut barrer
 
 
 def main():
@@ -13,3 +25,6 @@ def main():
         for joueur in joueurs:
             jouer(iTour,joueur)
     finir_partie()
+
+if __name__ == "__main__":
+    main()
