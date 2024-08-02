56d3c3b6e1194e1b2c129d001bd7cc5f8bcec178
Pierre NEILLO <pierre.neillo@free.fr>
2024-08-02 14:51:15 +0200

    Logique: structure de `Coup` continuée

--- logique.py
+++ logique.py
@@ -2,7 +2,28 @@ from constantes import *
 
 
 class Coup:
-    coups = []
+    coups = []  # Tous les coups possibles
 
-    def __init__(self, restrictions):
+    def __init__(self, nom, restrictions):
         self.restrictions = restrictions
+        self.nom = nom
+
+    def possible(self, des):
+        """Renvoie un booléen indiquant si le lancer de dés correspond au coup"""
+
+    def score(self, des):
+        """Prend en paramètre un lancer de dés correspondant au coup, et renvoie le score obtenu pour ce coup"""
+
+    @classmethod
+    def coups_possibles(cls, des):
+        c_pos = []
+        for c in Coup.coups:
+            if c.possible(des):
+                c_pos.append(c)
+        return c
+
+    def __str__(self):
+        return self.nom
+
+    def __repr__(self):
+        return str(self)
