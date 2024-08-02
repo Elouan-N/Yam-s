31c98bac99accf2e1d03931b9c512a6146cdb249
Elouan-N <148973555+Elouan-N@users.noreply.github.com>
2024-08-02 14:36:35 +0200

    structure de base

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from constantes import *
+from graphisme import *
+from logique import *
 
+
+def jouer():
+    pass
+
+
+def main():
+    init_partie()
+    for iTour in range(NB_TOURS):
+        for joueur in joueurs:
+            jouer(iTour,joueur)
+    finir_partie()
