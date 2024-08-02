e2fcc18cdd067efb1e9f26b0ddf8ab3da0890af4
Pierre NEILLO <pierre.neillo@free.fr>
2024-08-02 14:59:39 +0200

    Graphisme afficher_des

--- graphisme.py
+++ graphisme.py
@@ -1,1 +1,6 @@
 from constantes import *
+
+
+def afficher_des(des_conserves: list[int], des_tires: list[int]) -> None:
+    print("Dés conservés:", " ".join(map(str, sorted(des_conserves))))
+    print("Dés tirés", " ".join(map(str, sorted(des_tires))))
