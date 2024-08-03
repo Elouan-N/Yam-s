ed719f8e1ec51f2c1d3eab330160e10c2eb59f72
Elouan-N <148973555+Elouan-N@users.noreply.github.com>
2024-08-03 09:28:53 +0200

    fonction est_inclus

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from constantes import *
 from graphisme import *
 from logique import *
-
 from random import randint
+import copy
 
 joueurs = []
 
@@ -22,7 +22,7 @@ class Joueur:
 
 def init_partie():
     init_logique()
-    init_graphismes()
+    init_graphisme()
 
 def finir_partie():
     pass
@@ -32,6 +32,21 @@ def tirage(n):
     for _ in range(n):
         des_tires.append(randint(1,6))
     return des_tires
+
+def est_inclus(a :list, b :list) -> bool:
+    """Renvoie True ssi a est inclus dans b"""
+    _b = copy.deepcopy(b)
+    for elt in a:
+        if elt not in _b:
+            return False
+        else:
+            _b.pop(_b.index(elt))
+    return True
+
+def get_des_conserves(des_conserves, des_tires):
+    while not est_inclus(des_conserves_choisis := list(map(int, input("Quels dés voulez-vous conserver?\n>>> ").split())),des_conserves+des_tires) :
+        print("Choix impossible, veuillez réessayer")
+    return des_conserves_choisis
 
 def jouer(iTour, joueur):
     des_conserves = []
